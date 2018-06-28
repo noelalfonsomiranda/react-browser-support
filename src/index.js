458610d80dcd7f08653574dc6aea0fd0e0576ed3
noelalfonsomiranda <noelalfonsomiranda@gmail.com>
2018-06-28 14:30:20 +0800

    git push update

--- src/index.js
+++ src/index.js
@@ -9,66 +9,73 @@ export const detectBrowser = data => {
   
   return {
     ...detect(),
-    message: data && data.message,
-    supported: data && data.supported
+    message: data.message,
+    supported: data.supported
   }
 }
 export default class BrowserSupport extends Component {
-    static propTypes = {
-      supported: PropTypes.object.isRequired,
-    }
-    state = {
-      browser: {},
-      message: '',
-      supported: true,
-    }
-    componentDidMount() {
-      const browser = detect();
-      this.determineBrowserSupport(browser);
-    }
-    render() {
-      let { children, className, style } = this.props;
+  static propTypes = {
+    supported: PropTypes.object.isRequired,
+  }
 
-      return this.state && !this.state.supported ? (
-        <div
-          className={(!style) ? (className || 'warning-callout') : ''}
-          style={style || {}}>
-          {children ? children : (
-              <div>{this.state.message}</div>
-          )}
-        </div>
-      ) : null
+  state = {
+    browser: {},
+    message: '',
+    supported: true,
+  }
+
+  determineBrowserSupport(browser) {
+    let { supported } = this.props;
+    if (!browser) {
+        console.log('could not detect browser');
     }
-    determineBrowserSupport(browser) {
-      let { supported } = this.props;
-      if (!browser) {
-          console.log('could not detect browser');
-      }
-      else {
-        if (!supported[browser.name]) {
+    else {
+      if (!supported[browser.name]) {
+        this.setAsUnsupported(browser);
+      } else {
+        let browserVersion = supported[browser.name];
+        if (cmp(browser.version, browserVersion) < 0) {
           this.setAsUnsupported(browser);
         } else {
-          let browserVersion = supported[browser.name];
-          if (cmp(browser.version, browserVersion) < 0) {
-            this.setAsUnsupported(browser);
-          } else {
-              this.setAsSupported(browser);
-          }
+            this.setAsSupported(browser);
         }
       }
     }
-    setAsUnsupported(browser) {
-      this.setState({
-        browser,
-        supported: false,
-        message: `${browser.name} version ${browser.version} is not currently supported`,
-      }, () => detectBrowser(this.state))
-    }
-    setAsSupported(browser) {
-      this.setState({
-        browser,
-        supported: true,
-        message: `${browser.name} version ${browser.version} is supported`
-      }, () => detectBrowser(this.state))
-    }
+  }
+
+  setAsUnsupported(browser) {
+    this.setState({
+      browser,
+      supported: false,
+      message: `${browser.name} version ${browser.version} is not currently supported`,
+    }, () => detectBrowser(this.state))
+  }
+
+  setAsSupported(browser) {
+    this.setState({
+      browser,
+      supported: true,
+      message: `${browser.name} version ${browser.version} is supported`
+    }, () => detectBrowser(this.state))
+  }
+
+  componentDidMount() {
+    const browser = detect();
+    this.determineBrowserSupport(browser);
+  }
+  
+  render() {
+    let { children, className, style } = this.props;
+
+    return this.state && !this.state.supported ? (
+      <div
+        className={(!style) ? (className || 'warning-callout') : ''}
+        style={style || {}}>
+        {children ? children : (
+            <div>{this.state.message}</div>
+        )}
+      </div>
+    ) : null
+  }
+    
 }
