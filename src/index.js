7471b353dd3608640c659e3e9b95d33bd4d54433
noelalfonsomiranda <noelalfonsomiranda@gmail.com>
2018-06-28 14:04:01 +0800

    update

--- src/index.js
+++ src/index.js
@@ -65,9 +65,10 @@ export default class BrowserSupport extends Component {
 }
 
 export const detectBrowser = data => {
+    console.log('data', data)
     return {
         ...detect(),
-        message: data && data.message,
-        supported: data && data.supported
+        message: data.message,
+        supported: data.supported
     }
 }
